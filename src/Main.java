0a6f60d395b4a9a3c6c49fb982a2196f39757125
Adrian Herrera <adrian.herrera@salesianos-lacuesta.net>
2024-04-02 12:20:39 +0100

    feat(Main): ask number to user and print it

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("¡¡Hola Mundo!!");
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Introduce un numerito: ");
+    int askedNumber = scanner.nextInt();
+
+    System.out.println("¡¡Hola Mundo!! Tu número es: " + askedNumber);
+    
+    scanner.close();
   }
 }
